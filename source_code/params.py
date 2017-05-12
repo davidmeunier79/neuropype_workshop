158764d2bebf29f289a3e5ce43367e012932df62
Annalisa Pascarella <a.pascarella@iac.cnr.it>
2017-05-12 11:21:09 -0400

    updated params

--- source_code/params.py
+++ source_code/params.py
@@ -5,12 +5,16 @@ if getpass.getuser() == 'david':
         
     main_path = '/mnt/Data/Omega/' ### David
     subject_ids = ['sub-0003']
+elif getpass.getuser() == 'pasca':
+  
+    main_path = '/run/media/pasca/paska/meg_data/omega/sample_BIDS_omega/'  ### Annalisa
+    subject_ids = ['sub-0003']
 else:
     
-    main_path = '/run/media/pasca/paska/meg_data/omega/sample_BIDS_omega/' ### AnnaLisa
+    #TODO set here your data path!!!
+    main_path = ''  
     subject_ids = ['sub-0003', 'sub-0004', 'sub-0006']
 
 sessions = ['ses-0001']
-
 
 data_path = main_path
