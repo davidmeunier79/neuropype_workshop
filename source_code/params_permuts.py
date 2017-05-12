8ea286e20fcae35d281add4d208470601fa612d9
David Meunier (HP-EliteBook) <david.meunier@inserm.fr>
2017-05-12 23:24:34 +0200

    ref_labels et coords dans params_conn

--- source_code/params_permuts.py
+++ source_code/params_permuts.py
@@ -7,9 +7,16 @@ import os
 
 nb_permuts = 100
 
-alpha = 1.0/float(nb_permuts)
+#alpha = 1.0/float(nb_permuts)
+alpha = 0.05
 
-mean_spectral_permut_analysis_name = spectral_analysis_name + "_permuts_" + str(nb_permuts)
+#mean_spectral_permut_analysis_name = spectral_analysis_name + "_permuts_" + str(nb_permuts)
 
-ref_labels_file = os.path.join(main_path,"correct_channel_names.txt")
-ref_coords_file = os.path.join(main_path,"correct_channel_coords.txt")
+#ref_labels_file = os.path.join(main_path,"correct_channel_names.txt")
+#ref_coords_file = os.path.join(main_path,"correct_channel_coords.txt")
+
+##### graph ####
+mean_spectral_permut_analysis_name = spectral_analysis_name + "_rada_permuts_" + str(nb_permuts)
+
+mean_radatools_optim = "WS tfrf 1"
+mean_con_den = 0.1
