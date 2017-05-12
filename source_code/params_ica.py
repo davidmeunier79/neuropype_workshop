23977596f4c9cbd21713ca966becfe0e9d2ee75c
Annalisa Pascarella <a.pascarella@iac.cnr.it>
2017-05-12 11:26:19 -0400

    updated params

--- source_code/params_ica.py
+++ source_code/params_ica.py
@@ -4,12 +4,6 @@ from params import *
 
 data_type = 'ds'
 
-#main_path = '/run/media/pasca/paska/meg_data/omega/sample_BIDS_omega/'
-#data_path = main_path
-
-#subject_ids = ['sub-0003', 'sub-0004', 'sub-0006']
-#sessions = ['ses-0001']
-
 down_sfreq = 800
 l_freq = 0.1
 h_freq = 150
@@ -17,7 +11,7 @@ h_freq = 150
 
 # ------------------------ SET ICA variables -----------------------------#
 
-is_ICA = True  # if True we apply ICA to remove ECG and EoG artifacts
+is_ICA = True  # True if we apply ICA to remove ECG and EoG artifacts
 
 # specify ECG and EoG channel names if we know them
 ECG_ch_name = 'ECG'
@@ -26,5 +20,5 @@ variance = 0.95
 
 reject = dict(mag=5e-12, grad=5000e-13)
 
-# pipeline directory within the main_path dir
+# workflow directory within the main_path dir
 preproc_pipeline_name = 'preprocessing_pipeline'
